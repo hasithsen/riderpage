d074e02bfb9f293f88f09b42b6b06d4f5d1fa9b4
hasithsen <sen.hasith@gmail.com>
2024-04-23 07:36:19 +0000

    feat: Add django cookiecutter project

---
+++ riderpage/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from riderpage.users.models import User
+from riderpage.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
